cb60ca7433e8dd8267f0dd7dcfe6ae72a9a0c245
prakash2512 <64909867+prakash2512@users.noreply.github.com>
2021-03-18 15:43:36 +0530

    Update G-M-J-D-jen.sh

--- G-M-J-D-jen.sh
+++ G-M-J-D-jen.sh
@@ -22,10 +22,10 @@ yum install docker -y
 sudo chkconfig docker on
 
 echo "Install Jenkins"
-wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
-rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
-yum install -y jenkins
-sudo usermod -a -G docker jenkins
-sudo chkconfig jenkins on
-sudo service docker start
-sudo service jenkins start
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+sudo yum upgrade
+sudo yum install jenkins java-1.8.0-openjdk-devel
+sudo systemctl start jenkins
+sudo systemctl enable jenkins
