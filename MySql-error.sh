a78bc285207d6ff11cb7af2602ca80a16d227ed1
prakash2512 <64909867+prakash2512@users.noreply.github.com>
2021-09-07 11:05:07 +0530

    Update MySql-error.sh

--- MySql-error.sh
+++ MySql-error.sh
@@ -3,6 +3,24 @@ sudo systemctl stop mysql
 sudo apt-get purge mysql-server mysql-client mysql-common mysql-server-core-* mysql-client-core-*
 sudo rm -rf /etc/mysql /var/lib/mysql
 
+###To create db, username and password
+
+create database databasename;
+grant all privileges on database.* to 'username'@'host' identified by 'password';
+flush privileges;
+exit;
+
+
+### TO see user's
+login to root
+use mysql
+select * from user;
+
+### To drop a user
+drop user 'username'@'localhost';
+
+
+
 
 
 
